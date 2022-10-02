43fc8266091b85b5c1b72224840e52e1c14ecdd7
giraffelim <taeyang7704@naver.com>
2022-10-02 15:42:07 +0900

    feat: 입력과 출력을 나타내는 Inputter, Printer 추가

---
+++ src/main/java/baseball/util/Inputter.java
@@ -0,0 +1,12 @@
+package baseball.util;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class Inputter {
+    private Inputter() {
+    }
+
+    public static String readLine() {
+        return Console.readLine();
+    }
+}
